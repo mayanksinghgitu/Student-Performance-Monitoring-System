bab902a0a4f49ac0ed1a734056b397f49dafc4c4
mayanksinghgitu <mayanksinghsara80@gmail.com>
2024-12-20 14:36:42 +0530

    Delete Student.java

--- Student.java
+++
@@ -1,9 +0,0 @@
-public class Student {
-    private int studentId;
-    private String firstName;
-    private String lastName;
-    private String class;
-    private String email;
-
-    // Getters and Setters
-}
